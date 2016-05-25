173e54bc869f61a900b19e8dbdb186c021df54ad
Pierre-Guillaume <pierreguillaumelaurin@gmail.com>
2016-05-25 15:40:28 -0400

    set up basic gulp environment

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	})
+})
+
+gulp.task('watch', ['browserSync'], function (){
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/css/**/*.css', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+})
