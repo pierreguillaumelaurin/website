72c758b90059b1a3f592b2040b98ec5922173633
Pierre-Guillaume <pierreguillaumelaurin@gmail.com>
2016-05-30 16:48:44 -0400

    Fix buttons

--- app/js/script.js
+++ app/js/script.js
@@ -2,25 +2,31 @@
 $(function() {
     var targetOffset = $('#mba').offset().top - window.innerHeight;
     var scrolling = false;
-    $(window).scroll(function(){
+    if(!scrolling) {
+      $(window).scroll(function(){
         
-        if (!scrolling)
-        {
         var currentOffset = $(document).scrollTop();
         console.log( targetOffset + ' - ' + currentOffset );   //just for your debugging
         (function(){
-        while ( currentOffset > targetOffset - 50 && currentOffset < targetOffset + 50){
-            setInterval(flash(), 2000);
+        if ( currentOffset > targetOffset){
+            setInterval(flash, 1000);
           }
+        
         })();
-        scrolling=true;
-    };
-  });
+      }
+    });
 });
+*/
+
+/*
+setInterval(flash, 400);
+
+
 
 function flash () {
   $('#mba').toggleClass('reverse-bg-color');
+  scrolling = true
   console.log('toggliing on!');
-  scrolling = false;
 }
 */
+
